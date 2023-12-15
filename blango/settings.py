68379fe0e865335626b0df7ab949d65b70fdcd4d
Muhammad Muhammad <husseinkhan237@gmail.com>
2023-12-15 12:16:12 +0000

    Finish django registration

--- blango/settings.py
+++ blango/settings.py
@@ -170,6 +170,9 @@ class Dev(Configuration):
     CRISPY_TEMPLATE_PACK = "bootstrap5"
     INTERNAL_IPS = ["192.168.11.179"]
     AUTH_USER_MODEL = "blango_auth.User"
+    EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+    ACCOUNT_ACTIVATION_DAYS = 7
+    # REGISTRATION_OPEN = False
 
 class Prod(Dev):
   
