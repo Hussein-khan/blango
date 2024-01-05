04e49c002f2fcbee01acbce4dd2833d793fa9bd2
Muhammad Muhammad <husseinkhan237@gmail.com>
2024-01-05 05:00:05 +0000

    finish simplejwt

--- blango/settings.py
+++ blango/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import os
 from configurations import Configuration, values
 import dj_database_url
+from datetime import timedelta
 
 
 class Dev(Configuration):
@@ -81,6 +82,7 @@ class Dev(Configuration):
         "rest_framework.authentication.BasicAuthentication",
         "rest_framework.authentication.SessionAuthentication",
         "rest_framework.authentication.TokenAuthentication",
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
       ],
     "DEFAULT_THROTTLE_CLASSES": [
         "blog.api.throttling.AnonSustainedThrottle",
@@ -101,6 +103,11 @@ class Dev(Configuration):
         "django_filters.rest_framework.DjangoFilterBackend",
         "rest_framework.filters.OrderingFilter"
     ],
+    }
+
+    SIMPLE_JWT = {
+        "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
+        "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
     }
     SWAGGER_SETTINGS = {
         "SECURITY_DEFINITIONS": {
